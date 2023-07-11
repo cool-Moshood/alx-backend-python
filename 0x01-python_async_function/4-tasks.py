f416cdd0a5382ca393cbab12927b5b1d38ec118c
cool-Moshood <moshoodage@gmail.com>
2023-07-11 20:48:33 +0100

    task 4

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Executing multiple coroutines"""
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """return a list of values in async"""
+
+    seconds_list = []
+    for _ in range(0, n):
+        seconds_list.append(task_wait_random(max_delay))
+    t = await asyncio.gather(*seconds_list)
+    return sorted(t)
