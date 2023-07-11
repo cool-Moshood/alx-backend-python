9d1b3abe664df0cab915810fdb867899d476ff76
cool-Moshood <moshoodage@gmail.com>
2023-07-11 20:55:52 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" an asynchronous coroutine that takes in
-two integer argument"""
+"""An asynchronous coroutine that takes in two integer arguments"""
 import random
 import asyncio
 from typing import List
@@ -8,10 +7,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """return a list of values in async"""
+    """Return a list of values asynchronously"""
 
     sec_list = []
     for _ in range(0, n):
         sec_list.append(wait_random(max_delay))
-        srt = await asyncio.gather(*sec_list)
-        return sorted(srt)
+    srt = await asyncio.gather(*sec_list)
+    return sorted(srt)
